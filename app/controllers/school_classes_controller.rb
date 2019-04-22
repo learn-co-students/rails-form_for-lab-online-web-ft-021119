4cfa9ca2193c11e9c313f809bfc5c8e9cc79f790
github username <github email address>
2019-04-22 00:30:53 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,6 +4,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    #byebug
     @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
@@ -22,4 +23,10 @@ class SchoolClassesController < ApplicationController
     @school_class.update(params.require(:school_class).permit(:title, :room_number))
     redirect_to school_class_path(@school_class)
   end
+
+    private
+    def set_school_class
+      @school_class = SchoolClass.find_by_id(params[:id])
+    end
+
 end
