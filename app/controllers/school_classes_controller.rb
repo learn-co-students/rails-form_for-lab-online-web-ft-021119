013e0b2c83c74720f01164dd0dfec8ec70cf3612
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-24 10:50:40 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,29 @@
 class SchoolClassesController < ApplicationController
+    # def new
 
+    #     end
+
+        def create
+            @schoolclass = SchoolClass.new(schoolclass_params(:title, :room_number))
+            @schoolclass.save
+            redirect_to school_classes_path(@schoolclass)
+        end
+
+    #     def show
+
+    #     end
+
+    #     def edit
+
+    #     end
+
+    #     def update
+
+    #     end
+
+        private
+
+        def schoolclass_params(*args)
+            params.require(:school_class).permit(*args)
+        end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,28 @@
 class StudentsController < ApplicationController
+# new, create, show, edit, and update actions in each of the controllers.
+    def new
 
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    private
+
+    def student_params(*args)
+        params.require(:student).permit(*args)
+    end
 end
